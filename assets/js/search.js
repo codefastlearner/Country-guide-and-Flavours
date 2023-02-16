55912809b8fc682efa65ec94e7195b230fa8f95a
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-02-16 21:35:07 +0000

    added second API functionality

--- assets/js/search.js
+++ assets/js/search.js
@@ -12,13 +12,14 @@ $(".zone").html("Time zone:" + " " + countryData[6]);
 
 console.log(countryData);
 
-$("#flavour").on("click", () => {
-  let counrtryRecipie = $("#search-name");
+$("#explore").on("click", () => {
+  let countryRecipie = $("#search-name").text().slice(5); // need to grab the title from the card
+  console.log(countryRecipie);
 
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "0fdcc016b6mshf840d243807abbep13d740jsndfb2849b2ade",
+      "X-RapidAPI-Key": "c0e654e005msh4c9a90bb5005fbbp1284cdjsn75b2a3c5f094",
       "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
     },
   };
@@ -26,9 +27,55 @@ $("#flavour").on("click", () => {
   fetch("https://themealdb.p.rapidapi.com/list.php?a=list", options)
     .then((response) => response.json())
     .then((data) => {
-      for (let dishes of data) {
-        console.log(dishes.strArea);
+      console.log(data);
+      let matchingNationality = "";
+
+      for (let nationality of data.meals) {
+        const currentNationality = nationality.strArea;
+        // grab first 3 letters of search term
+        let firstThreeSearch = countryRecipie.slice(0, 4).trim();
+        let firstThreeNationality = currentNationality.slice(0, 3).trim(); // added trim no match bc of whitespace
+
+        if (firstThreeSearch == firstThreeNationality) {
+          matchingNationality = currentNationality;
+          console.log("updated matching Nationalty: " + matchingNationality);
+        }
       }
+      console.log(matchingNationality);
+      // make the second API call below using the value stored in matchingNationality variable
+      displayCountryRecipie(matchingNationality);
     })
     .catch((err) => console.error(err));
 });
+
+// handles the second API call
+function displayCountryRecipie(nationality){
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '0fdcc016b6mshf840d243807abbep13d740jsndfb2849b2ade',
+      'X-RapidAPI-Host': 'themealdb.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://themealdb.p.rapidapi.com/filter.php?a=' + nationality, options)
+    .then(response => response.json())
+    .then(data => {
+
+    
+    for(let i = 0; i < 4; i++){
+      let dishIcon = data.meals[i].strMealThumb;
+      let dishName = data.meals[i].strMeal;
+
+      $(`#${i}`).attr("src", dishIcon);
+      $(`#t${i}`).html(dishName);
+  }
+
+    })
+    
+
+
+
+
+}
+
